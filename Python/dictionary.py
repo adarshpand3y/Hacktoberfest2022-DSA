22712cd1fcaf26d953d8d00a82e97336433dc354
unknown <nasyithmusyafa@gmail.com>
2022-10-15 09:21:49 +0700

    Create dictionary.py

---
+++ Python/dictionary.py
@@ -0,0 +1,6 @@
+#Dictionary in Python
+my_dict = {'First': 'Python', 'Second': 'Java', 'Third': 'Ruby'}
+print(my_dict.keys()) #get keys
+print(my_dict.values()) #get values
+print(my_dict.items()) #get key-value pairs
+print(my_dict.get('First'))
