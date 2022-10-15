0891bb4d8d480b7801a595d170f9896e9730b0f4
Himanshu Gupta <59887776+Himanshu-Gupta2001@users.noreply.github.com>
2022-10-15 22:15:31 +0530

    Create ReachNthStairs.java

---
+++ Java/Recursion/ReachNthStairs.java
@@ -0,0 +1,46 @@
+import java.lang.*;
+
+import java.util.*;
+ 
+
+public class GfG {
+ 
+
+    // Returns count of ways to reach
+
+    // n-th stair using 1 or 2 or 3 steps.
+
+    public static int findStep(int n)
+
+    {
+
+        if ( n == 0)
+
+            return 1;
+
+        else if (n < 0)
+
+            return 0;
+ 
+
+        else
+
+            return findStep(n - 3) + findStep(n - 2)
+
+                + findStep(n - 1);
+
+    }
+ 
+
+    // Driver function
+
+    public static void main(String argc[])
+
+    {
+
+        int n = 4;
+
+        System.out.println(findStep(n));
+
+    }
+}
