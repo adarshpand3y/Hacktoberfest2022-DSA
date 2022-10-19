b6fdfabce574ef3cdf9c6931116d6da8df88d1e8
Nidhi Verma <96578258+nidhirk2020@users.noreply.github.com>
2022-10-19 16:55:08 +0530

    Compare_String.c
    * if (string1 > string2) it returns a positive value.
    * if both the strings are equal lexicographically
    i.e.(string1 == string2) it returns 0.
    * if (string1 < string2) it returns a negative value.

---
+++ Java/Strings/Compare_Strings.java
@@ -0,0 +1,17 @@
+package strings;
+import java.util.*;
+
+public class compare_string{ 
+
+    public static void main(String args[]) 
+    { 
+        String string1 = new String("Kurukshetra University"); 
+        String string2 = new String("Electronics"); 
+
+  
+        // Comparing for String 1 < String 2 
+        System.out.println("Comparing " + string1 + " and " + string2 
+                           + " : " + string1.compareTo(string2)); 
+
+    } 
+} 
