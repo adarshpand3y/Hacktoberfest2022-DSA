4f2e499a4fc8d7d3f779b89f3e9cc5bb3292b7b9
Hardik Gupta <29226479+Hardikg13@users.noreply.github.com>
2022-10-22 04:07:12 +0530

    Create Dictionary.py

---
+++ Python/Dictionary.py
@@ -0,0 +1,12 @@
+phone = input(" Phone:")
+digit_mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": " Four"
+}
+output =" "
+for ch in phone:
+    output +=digit_mapping.get(ch, "@") + " "
+print(output)
+# try out multiple permutations,its super fun!
