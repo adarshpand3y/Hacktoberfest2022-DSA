4bf434daf19553c686b6837cbc8ab119703274eb
Armaan2807 <89082945+Armaan2807@users.noreply.github.com>
2022-10-23 22:13:48 -0700

    Add files via upload

---
+++ Javascript/TwoArraySumCp.js
@@ -0,0 +1,18 @@
+// Problem statement
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// solution
+function twoSum(nums, target) {
+    let numObj = {};
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (numObj[complement] !== undefined) {
+            return [numObj[complement], i];
+        }
+        numObj[nums[i]] = i;
+    }
+}
