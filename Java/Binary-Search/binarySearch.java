5395472c14ec8e55633e3f0a0ad5835cd4946738
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2022-10-25 16:34:01 +0530

    created binarySearch.java in Binary-Search folder

--- Java/Binary-Search/binarySearch.java
+++ Java/Binary-Search/binarySearch.java
@@ -2,37 +2,35 @@
 public class binarySearch {
 
     public static void main(String[] args) {
-        int[] arr = {11,23,45,67,78,89,90};
-        int target = 7;
-        System.out.println(search(arr, target);
-    }
-
-
-    static void search(int [] arr, target){
-        int s = 0;
-        int e = arr.length -1 ;
-        while(s <= e)
-        {
-
-            if(target < arr[0]){
-                return -1;
+            int[] arr= {90, 56, 45, 34, 23, 12, 11, 7, -1, -5};
+            int target= 45;
+            int ans= binarySearch(arr, target);
+            System.out.println(ans);
+        }
+    
+        //return the index
+        static int binarySearch (int[] arr, int target){
+            int start= 0;
+            int end= arr.length -1;
+    
+            while (start <= end){
+                //return middle element
+                int mid= start + (end-start)/ 2;
+    
+            if (target < arr[mid]){
+                start= mid + 1;
             }
-            int mid = s + (e-s)/2;
-            if(target == arr[mid])
-            {
+            else if (target > arr[mid]){
+                end= mid -1;
+                    }
+            else {
                 return mid;
             }
-            else if(target > arr[mid])
-            {
-                s = mid+1;
-            }
-
-            else {
-                e = mid-1;
-            }
         }
-        return mid;
+        //this means element doesnt exist
+        return -1;
     }
+    
 
 
     //CEILING AND FLOOR
